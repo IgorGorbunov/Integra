52559bc7ffb1ee9e82d2b8296df8ec598635286b
Igor Gorbunov <give@list.ru>
2017-02-04 19:59:46 +0400

    add resize interface in IntgrResultsForm

--- Integra/IntgrResultsForm.h
+++ Integra/IntgrResultsForm.h
@@ -69,6 +69,11 @@ namespace Integra {
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column3;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column4;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column2;
+	private: System::Windows::Forms::Panel^  panel1;
+	private: System::Windows::Forms::Panel^  panel2;
+	private: System::Windows::Forms::Panel^  panel3;
+	private: System::Windows::Forms::Panel^  panel4;
+	private: System::Windows::Forms::Panel^  panel5;
 
 
 	private:
@@ -149,7 +154,17 @@ namespace Integra {
 			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
 			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
 			this->bClose = (gcnew System::Windows::Forms::Button());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->panel3 = (gcnew System::Windows::Forms::Panel());
+			this->panel4 = (gcnew System::Windows::Forms::Panel());
+			this->panel5 = (gcnew System::Windows::Forms::Panel());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->dgv))->BeginInit();
+			this->panel1->SuspendLayout();
+			this->panel2->SuspendLayout();
+			this->panel3->SuspendLayout();
+			this->panel4->SuspendLayout();
+			this->panel5->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// dgv
@@ -159,10 +174,11 @@ namespace Integra {
 			this->dgv->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
 			this->dgv->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(8) {this->Column1, this->Column7, 
 				this->Column8, this->Column5, this->Column6, this->Column3, this->Column4, this->Column2});
-			this->dgv->Location = System::Drawing::Point(20, 134);
+			this->dgv->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->dgv->Location = System::Drawing::Point(10, 10);
 			this->dgv->Name = L"dgv";
 			this->dgv->ReadOnly = true;
-			this->dgv->Size = System::Drawing::Size(847, 150);
+			this->dgv->Size = System::Drawing::Size(845, 205);
 			this->dgv->TabIndex = 0;
 			// 
 			// Column1
@@ -215,9 +231,9 @@ namespace Integra {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(699, 71);
+			this->button1->Location = System::Drawing::Point(21, 13);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(168, 48);
+			this->button1->Size = System::Drawing::Size(249, 23);
 			this->button1->TabIndex = 24;
 			this->button1->Text = L"Просмотр результатов интеграции";
 			this->button1->UseVisualStyleBackColor = true;
@@ -225,7 +241,7 @@ namespace Integra {
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(255, 71);
+			this->label5->Location = System::Drawing::Point(248, 71);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(22, 13);
 			this->label5->TabIndex = 23;
@@ -234,7 +250,7 @@ namespace Integra {
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(36, 71);
+			this->label4->Location = System::Drawing::Point(29, 71);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(150, 13);
 			this->label4->TabIndex = 22;
@@ -243,7 +259,7 @@ namespace Integra {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(314, 16);
+			this->label3->Location = System::Drawing::Point(307, 16);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(107, 13);
 			this->label3->TabIndex = 21;
@@ -252,7 +268,7 @@ namespace Integra {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(186, 16);
+			this->label2->Location = System::Drawing::Point(179, 16);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(103, 13);
 			this->label2->TabIndex = 20;
@@ -261,7 +277,7 @@ namespace Integra {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(26, 16);
+			this->label1->Location = System::Drawing::Point(19, 16);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(97, 13);
 			this->label1->TabIndex = 19;
@@ -269,14 +285,14 @@ namespace Integra {
 			// 
 			// dateTimePicker2
 			// 
-			this->dateTimePicker2->Location = System::Drawing::Point(235, 95);
+			this->dateTimePicker2->Location = System::Drawing::Point(228, 95);
 			this->dateTimePicker2->Name = L"dateTimePicker2";
 			this->dateTimePicker2->Size = System::Drawing::Size(200, 20);
 			this->dateTimePicker2->TabIndex = 18;
 			// 
 			// dateTimePicker1
 			// 
-			this->dateTimePicker1->Location = System::Drawing::Point(20, 94);
+			this->dateTimePicker1->Location = System::Drawing::Point(13, 94);
 			this->dateTimePicker1->Name = L"dateTimePicker1";
 			this->dateTimePicker1->Size = System::Drawing::Size(200, 20);
 			this->dateTimePicker1->TabIndex = 17;
@@ -284,7 +300,7 @@ namespace Integra {
 			// comboBox3
 			// 
 			this->comboBox3->FormattingEnabled = true;
-			this->comboBox3->Location = System::Drawing::Point(317, 38);
+			this->comboBox3->Location = System::Drawing::Point(310, 38);
 			this->comboBox3->Name = L"comboBox3";
 			this->comboBox3->Size = System::Drawing::Size(121, 21);
 			this->comboBox3->TabIndex = 16;
@@ -292,7 +308,7 @@ namespace Integra {
 			// comboBox2
 			// 
 			this->comboBox2->FormattingEnabled = true;
-			this->comboBox2->Location = System::Drawing::Point(180, 38);
+			this->comboBox2->Location = System::Drawing::Point(173, 38);
 			this->comboBox2->Name = L"comboBox2";
 			this->comboBox2->Size = System::Drawing::Size(121, 21);
 			this->comboBox2->TabIndex = 15;
@@ -300,14 +316,14 @@ namespace Integra {
 			// comboBox1
 			// 
 			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(20, 38);
+			this->comboBox1->Location = System::Drawing::Point(13, 38);
 			this->comboBox1->Name = L"comboBox1";
 			this->comboBox1->Size = System::Drawing::Size(121, 21);
 			this->comboBox1->TabIndex = 14;
 			// 
 			// bClose
 			// 
-			this->bClose->Location = System::Drawing::Point(792, 307);
+			this->bClose->Location = System::Drawing::Point(20, 13);
 			this->bClose->Name = L"bClose";
 			this->bClose->Size = System::Drawing::Size(75, 23);
 			this->bClose->TabIndex = 25;
@@ -315,30 +331,83 @@ namespace Integra {
 			this->bClose->UseVisualStyleBackColor = true;
 			this->bClose->Click += gcnew System::EventHandler(this, &IntgrResultsForm::bClose_Click);
 			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Controls->Add(this->comboBox1);
+			this->panel1->Controls->Add(this->comboBox2);
+			this->panel1->Controls->Add(this->label5);
+			this->panel1->Controls->Add(this->comboBox3);
+			this->panel1->Controls->Add(this->label4);
+			this->panel1->Controls->Add(this->dateTimePicker1);
+			this->panel1->Controls->Add(this->label3);
+			this->panel1->Controls->Add(this->dateTimePicker2);
+			this->panel1->Controls->Add(this->label2);
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panel1->Location = System::Drawing::Point(0, 0);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(865, 128);
+			this->panel1->TabIndex = 26;
+			// 
+			// panel2
+			// 
+			this->panel2->Controls->Add(this->panel3);
+			this->panel2->Controls->Add(this->button1);
+			this->panel2->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->panel2->Location = System::Drawing::Point(0, 353);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(865, 51);
+			this->panel2->TabIndex = 27;
+			// 
+			// panel3
+			// 
+			this->panel3->Controls->Add(this->bClose);
+			this->panel3->Dock = System::Windows::Forms::DockStyle::Right;
+			this->panel3->Location = System::Drawing::Point(751, 0);
+			this->panel3->Name = L"panel3";
+			this->panel3->Size = System::Drawing::Size(114, 51);
+			this->panel3->TabIndex = 0;
+			// 
+			// panel4
+			// 
+			this->panel4->Controls->Add(this->panel5);
+			this->panel4->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel4->Location = System::Drawing::Point(0, 128);
+			this->panel4->Name = L"panel4";
+			this->panel4->Size = System::Drawing::Size(865, 225);
+			this->panel4->TabIndex = 28;
+			// 
+			// panel5
+			// 
+			this->panel5->Controls->Add(this->dgv);
+			this->panel5->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel5->Location = System::Drawing::Point(0, 0);
+			this->panel5->Name = L"panel5";
+			this->panel5->Padding = System::Windows::Forms::Padding(10);
+			this->panel5->Size = System::Drawing::Size(865, 225);
+			this->panel5->TabIndex = 0;
+			// 
 			// IntgrResultsForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(886, 342);
-			this->Controls->Add(this->bClose);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->label5);
-			this->Controls->Add(this->label4);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
-			this->Controls->Add(this->dateTimePicker2);
-			this->Controls->Add(this->dateTimePicker1);
-			this->Controls->Add(this->comboBox3);
-			this->Controls->Add(this->comboBox2);
-			this->Controls->Add(this->comboBox1);
-			this->Controls->Add(this->dgv);
+			this->ClientSize = System::Drawing::Size(865, 404);
+			this->Controls->Add(this->panel4);
+			this->Controls->Add(this->panel2);
+			this->Controls->Add(this->panel1);
+			this->MinimumSize = System::Drawing::Size(670, 383);
 			this->Name = L"IntgrResultsForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterParent;
 			this->Text = L"Результаты интеграций";
 			this->Load += gcnew System::EventHandler(this, &IntgrResultsForm::IntgrResultsForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->dgv))->EndInit();
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->panel2->ResumeLayout(false);
+			this->panel3->ResumeLayout(false);
+			this->panel4->ResumeLayout(false);
+			this->panel5->ResumeLayout(false);
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
